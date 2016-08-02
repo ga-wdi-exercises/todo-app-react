32a712d3c8b9ba49f16b0169138ff724cf66ab18
Rebekah Milford <rmilford86@gmail.com>
2016-08-02 15:14:20 -0400

    making some progress

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
-// Developer TODO: add App component
+// bring in React and Component instance from react
+import React, {Component} from 'react'
+var ToDoApp = React.createClass({
+  getInitialState: function(){
+    return {items: []};
+  },
+  updateItems: function(newItem){
+    var allItems = this.state.items.concat([newItem]);
+    this.setState({items: allItems});
+
+  },
+  render: function(){
+    return (
+      <div>
+        <ToDoBanner/>
+        <TodoList item={this.state.items}/>
+        <TodoForm onFormSubmit={this.updateItems}/>
+      </div>
+    );
+  }
+});
+// // define our Hello component
+// class Hello extends Component {
+//   // what should the component render
+//   render () {
+//     // Make sure to return some UI
+//     return (
+//       <h1>Hello World!</h1>
+//     )
+//   }
+// }
+
+export default Hello
