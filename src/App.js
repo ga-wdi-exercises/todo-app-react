238c7bbb63913bcce3f729ddd9d8a46698a243b3
Adina Pollack <adinap16@gmail.com>
2016-08-02 17:19:14 -0400

    still working on add functionality

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
 // Developer TODO: add App component
+import React, {Component} from 'react'
+import Item from './Item.js'
+
+class Todo extends Component {
+  getInitialState() {
+      return {value: Item.state.body};
+    }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    }
+  render(){
+    let items = this.props.items.map((item, index) => (
+      <Item body={item} key={index} />
+    ))
+    return(
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>Items</h2>
+        {items}
+        <form>
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Post"/>
+        </form>
+
+      </div>
+    );
+  }
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from './App';
+import './index.css'
+
+const todo = {
+  title: "Adina's todos",
+  items: ["Todo 1", "Todo 2", "Todo 3", "Todo 4", "Todo 5"]
+}
+
+ReactDOM.render(
+  <Todo
+    title={todo.title}
+    items={todo.items}/>,
+    document.getElementById('root')
+);
