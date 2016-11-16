0482f60e34c8d306b7d4a36a82f4c3c601d7027d
Khoi Le <khoileee89@gmail.com>
2016-11-16 09:51:03 -0500

    initial view renders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Developer TODO: add application entry point
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Todos from "./Todos.js"
+
+ReactDOM.render(
+  <Todos />,
+  document.getElementById("root")
+)
