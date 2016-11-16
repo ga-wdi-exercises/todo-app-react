b4efc7fb51aa57fadd98d45794f25e99dab2d801
RYANBROPHY <BROPHY2321@ME.COM>
2016-11-16 09:44:46 -0500

    added div to enter task

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // Developer TODO: add App component
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render(){
+    return(
+
+      <div className="todoListMain">
+          <div className="header">
+            <form>
+              <input placeholder="enter task">
+              </input>
+              <button type="submit">add</button>
+            </form>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default ToDo;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDo from './App';
+
+ReactDOM.render(
+  <ToDo />,
+  document.getElementById('root')
+);
