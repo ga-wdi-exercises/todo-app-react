b4ebebfa8c944408b162859d1eb9848274309743
brown2206 <brown.angelo.jr@gmail.com>
2016-11-16 13:13:26 -0500

    worked on create input

--- src/create-todo.js
+++ src/create-todo.js
@@ -3,11 +3,17 @@ import React, {Component} from 'react'
 class CreateTodo extends Component {
   render() {
     return (
-      <form>
-        <input type="text" placeholder="What do I need to do?" />
+      <form onSubmit={this.handleCreate.bind(this)}>
+        <input type="text" placeholder="What do I need to do?" ref="createInput" />
         <button>Create</button>
       </form>
     )
+  }
+
+  handleCreate(event) {
+    event.preventDefault()
+
+    console.log(this.refs.createInput.value)
   }
 }
 
