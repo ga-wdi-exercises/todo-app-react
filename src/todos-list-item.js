2f32ccfeedaed558d893b6850dc942be45f3cfae
brown2206 <brown.angelo.jr@gmail.com>
2016-11-16 12:52:46 -0500

    created actions section

--- src/todos-list-item.js
+++ src/todos-list-item.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react'
 
 class TodosListItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isEditing: false
+    }
+  }
+
+  renderActionsSection() {
+    if (this.state.isEditing) {
+      return (
+        <td>
+          <button>Save</button>
+          <button>Cancel</button>
+        </td>
+      )
+    }
+
+    return (
+      <td>
+          <button onClick={this.onEditClick.bind(this)}>Edit</button>
+          <button>Delete</button>
+      </td>
+    )
+  }
+
   render() {
     return (
       <tr>
         <td>{this.props.task}</td>
-        <td>
-            <button>Edit</button>
-            <button>Delete</button>
-        </td>
+        {this.renderActionsSection()}
       </tr>
     )
+  }
+
+  onEditClick() {
+    this.setState({ isEditing: true })
   }
 }
 
