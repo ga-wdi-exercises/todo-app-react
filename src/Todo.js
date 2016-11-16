0b560a8e6ba382ab0e12f93991afcf2c4acf17fd
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-11-16 14:05:50 -0500

    form created

---
+++ src/Todo.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        Todo:
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+export default Todo
