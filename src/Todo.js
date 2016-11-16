62e07ba4b842a31ff2e3a15860c867fa45f3b831
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-11-16 09:23:09 -0500

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+// Developer TODO: add App component
+import React, { Component } from 'react'
+
+class Todo extends Component {
+  render(){
+    return(
+      <div className="main-container">
+        <h1>React Todo</h1>
+        <Todo/>
+      </div>
+    )
+  }
+}
+
+export default Todo
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Developer TODO: add application entry point
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Todo from './Todo'
+
+ReactDOM.render(
+  <Todo/>,
+  document.getElementById('root')
+)
