404594f0af4ed59f9d8d5dc11fe0de00390b54dd
brown2206 <brown.angelo.jr@gmail.com>
2016-11-16 11:29:52 -0500

    todos app setup works

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
-// Developer TODO: add App component
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <h1>React ToDos App Baaaaabbbyyy</h1>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+export default App
