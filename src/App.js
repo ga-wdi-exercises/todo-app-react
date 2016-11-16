29e8ca95f82e1c0cf2952778dfdb69737b7b3647
Alex Manno <alman92@gmail.com>
2016-11-16 15:56:21 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,1 +1,46 @@
 // Developer TODO: add App component
+import React, { Component } from 'react'
+import Item from './items.js'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        items: ["Dirty", "Naughty"],
+        newTodo: '',
+      }
+  }
+
+  handleSubmit(e){
+    let oldItems = this.state.items.concat([this.state.newTodo])
+    this.setState({
+      items: oldItems
+    })
+  }
+
+  handleChange(e){
+    this.setState({
+      newTodo: e.target.value
+      //.target saves the value
+    })
+  }
+
+  render() {
+    let items = this.state.items.map((item, i) => {
+      return <Item body={item} key={i} />
+    })
+    return (
+      <div>
+        <h1>To Do List</h1>
+        {items}
+        <form onSubmit={e => this.handleSubmit(e)}>
+          New To Do:
+            <input type="text" placeholder="New To Do..." value={this.state.newTodo} onChange={e => this.handleChange(e)}/>
+            <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
