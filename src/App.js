dfd5abf2267eb498fb79f7a2aadcaaa259acb44d
Nick Oki <nick.oki@live.com>
2016-11-16 09:41:44 -0500

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
-// Developer TODO: add App component
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+export default App
