09d48a435347ca6373f7f35c9392556c42df92bf
brown2206 <brown.angelo.jr@gmail.com>
2016-11-16 16:17:12 -0500

    finished product

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   render() {
     return (
     <div>
-      <h1>React ToDos App Baaaaabbbyyy</h1>
+      <h1>Honey Do List!!!</h1>
       <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)}/>
       <TodosList
           todos={this.state.todos}
