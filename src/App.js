f565d09a472d5f0bb193fd909117defbba662344
brown2206 <brown.angelo.jr@gmail.com>
2016-11-16 14:17:13 -0500

    edit functionality

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
       <TodosList
           todos={this.state.todos}
           toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
       />
     </div>
     )
@@ -46,6 +47,12 @@ class App extends Component {
       task,
       isCompleted: false
     })
+    this.setState({ todos: this.state.todos })
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask)
+    foundTodo.task = newTask
     this.setState({ todos: this.state.todos })
   }
 
--- src/todos-list-item.js
+++ src/todos-list-item.js
@@ -17,6 +17,16 @@ class TodosListItem extends Component {
       cursor: 'pointer'
     }
 
+    if (this.state.isEditing) {
+      return (
+        <td>
+            <form onSubmit={this.onSaveClick.bind(this)}>
+                <input type="text" defaultValue={task} ref="editInput" />
+            </form>
+        </td>
+      )
+    }
+
     return (
       <td style={taskStyle}
         onClick={this.props.toggleTask.bind(this, task)}
@@ -30,7 +40,7 @@ class TodosListItem extends Component {
     if (this.state.isEditing) {
       return (
         <td>
-          <button>Save</button>
+          <button onClick={this.onSaveClick.bind(this)}>Save</button>
           <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
         </td>
       )
@@ -59,6 +69,15 @@ class TodosListItem extends Component {
 
   onCancelClick() {
     this.setState({ isEditing: false })
+  }
+
+  onSaveClick(event) {
+      event.preventDefault()
+
+      const oldTask = this.props.task
+      const newTask = this.refs.editInput.value
+      this.props.saveTask(oldTask, newTask)
+      this.setState({ isEditing: false })
   }
 }
 
