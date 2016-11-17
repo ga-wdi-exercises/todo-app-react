770405117e0039cbbbf5e804e34de23d44f0f38b
Andrew Whitley <awhitley1233@gmail.com>
2016-11-17 09:01:14 -0500

    Todo Read complete

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 // Developer TODO: add App component
+
+import React, { Component } from 'react'
+import TodoList from './TodoList'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return(
+      <div className='app'>
+        <h1>TodoApp</h1>
+        <TodoList />
+      </div>
+    )
+  }
+}
+
+export default App
