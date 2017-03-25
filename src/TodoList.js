15490c4e894619c9e8b38a293d2c64be72636fe1
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-03-25 13:31:53 -0400

    functionality

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import Todos from './Todos'
+import Todos from './Todo'
+import AddTodoForm from './AddTodoForm'
 import seeds from '.seeds.js'
 
 class TodoList extends Component {
@@ -7,18 +8,87 @@ class TodoList extends Component {
     super (props)
     this.state = {
       title: 'Todos',
-      todos: seeds
+      todos: seeds,
+      addTodo: false,
+      newTodoTitle: '',
+      newTodoBody: ''
     }
   }
+
+  toggleNewTodo(e) {
+    e.preventDefault()
+    this.setState({
+      addTodo: true
+    })
+  }
+  setNewTodoTitle(e) {
+    console.log(e.target.value)
+    this.setState({
+      newTodoTitle: e.target.value
+    })
+  }
+  setNewTodoBody(e) {
+    console.log(e.target.value)
+    this.setState({
+      newTodoBody: e.target.value
+    })
+  }
+  addNewTodo(e) {
+    e.preventDefault()
+    let newTodo={
+      title: this.state.newTodoTitle,
+      body: this.state.newTodoBody,
+      completed: false
+    }
+    let newTodoSet = this.state.todos.slice()
+    newTodoSet.push(newTodo)
+    this.setState({
+      todos: newTodoSet,
+      addTodo: false,
+      newTodoTitle: '',
+      newTodoBody: ''
+    })
+    console.log(newTodo)
+  }
+
+  removeTodo(e) {
+    let index = parseInt(e.target.value, 10)
+    let newTodoSet = this.state.todos.slice()
+    newTodoSet.splice(e.target.value, 1)
+    this.setState({
+      todos: newTodoSet
+    })
+  }
+
   render() {
+    let todos = this.state.todos.map((todo, index) => {
+      return(
+
+
+        <Todo
+          todo={todo}
+          completed={ todo.completed }
+          key={index}
+          index={index}
+          removeTodo={ (e) => this.removeTodo(e)}
+        />
+      )
+    })
     return(
       <div className='todo-list'>
       <h2>{ this.state.title }</h2>
-      <Todos todos={ this.state.todos }/>
+      { todos }
+      <AddTodoForm
+        addTodo={ this.state.addTodo }
+        newTodo={ this.state.newTodo }
+        toggleNewTodo{ (e) => this.toggleNewTodo(e) }
+        setNewTodoTitle={ (e) => this.setNewTodoTitle(e) }
+        setNewTodoBody={ (e) => this.setNewTodoBody(e) }
+        addNewTodo={ (e) => this.addNewTodo(e)}
+      />
       </div>
     )
   }
-
 }
 
 export default TodoList
