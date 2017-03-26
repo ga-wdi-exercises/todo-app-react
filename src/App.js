617d5cce1785498a8842933aef1e67f50db40b1c
Habtamu Abere <habbuya@gmail.com>
2017-03-26 09:54:40 -0600

    start

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // Developer TODO: add App component
+import React, { Component } from 'react';
+import './App.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h2> Hello</h2>
+      </div>
+    );
+  }
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
