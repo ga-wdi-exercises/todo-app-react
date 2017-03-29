3961699635502356bce58ba54fdd0050dd13ddcd
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-29 16:16:09 -0400

    Todos showing

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// Developer TODO: add App component
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+// import Todo from "./Todo"
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      todos: [
+        {task: "Take out trash."},
+        {task: "Watch Netflix."},
+        {task: "Call mom."},
+        {task: "Play softball."},
+        {task: "Buy ketchup."}
+      ]
+    }
+  }
+  render(){
+    console.log(this.state.todos)
+    let todos = this.state.todos.map((todo, index) => {
+      return <div>{todo.task}</div>
+    })
+    return(
+      <div>
+        <h1>Todos</h1>
+        {todos}
+      </div>
+    )
+  }
+}
+
+export default App;
