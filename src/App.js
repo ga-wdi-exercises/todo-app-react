d26459ea4362fb11af3b960f4b05c805d37455da
Collin OConnell <ococncol@gmail.com>
2017-03-29 14:24:28 -0400

    able to make new todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import Todo from './Todo'
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
+      new: '',
       todos: [
         'first todo',
         'second todo',
@@ -11,13 +13,32 @@ class App extends Component {
       ]
     }
   }
+  newChange(e){
+    this.setState({
+      new: e.target.value
+    })
+  }
+
+  createTodo(e){
+    e.preventDefault()
+    this.setState({
+      todos: this.state.todos.concat([this.state.new])
+    })
+    this.refs.new.value = ''
+  }
   render() {
+    let todos = this.state.todos.map((todo, index) => {
+      return <Todo content={todo} key={index} />
+    })
     return (
+      <div>
+      <form onSubmit={e => this.createTodo(e)}>
+        <input type="text" placeholder="add a todo" ref="new" onChange={e => this.newChange(e)} />
+      </form>
       <ul>
-        <li>{this.state.todos[0]}</li>
-        <li>{this.state.todos[1]}</li>
-        <li>{this.state.todos[2]}</li>
+        {todos}
       </ul>
+      </div>
     )
   }
 }
