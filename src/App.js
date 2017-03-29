959899ca98bce94f5fa02aeb4fd8d1089863e803
Collin OConnell <ococncol@gmail.com>
2017-03-29 14:06:17 -0400

    able to views all todos

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-// Developer TODO: add App component
+import React, {Component} from 'react'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: [
+        'first todo',
+        'second todo',
+        'Brush up on Javascript'
+      ]
+    }
+  }
+  render() {
+    return (
+      <ul>
+        <li>{this.state.todos[0]}</li>
+        <li>{this.state.todos[1]}</li>
+        <li>{this.state.todos[2]}</li>
+      </ul>
+    )
+  }
+}
+export default App
