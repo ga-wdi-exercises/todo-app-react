5b97c5d09974678de5cb87c3676b4ca30ccbf016
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-30 00:31:28 -0400

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react'
+import CreateTodo from './CreateTodo'
+import TodosList from './TodosList'
+
+const todos = [
+{
+  task: 'go to chiptole for lunch',
+  isCompleted: true
+},
+{
+  task: 'react homework',
+  isCompleted: false
+},
+  {
+    task: 'learn redux',
+    isCompleted: false
+
+  },
+  {
+    task: 'go to meetup',
+    isCompleted: true
+  }
+]
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      todos:todos
+    }
+  }
   render(){
     return(
       <div>
-        <h1>React Todo App</h1> 
+        <h1>React Todo App</h1>
+          <CreateTodo createTask={this.createTask.bind(this)}/>
+          <TodosList todos={this.state.todos} createTask={this.createTask.bind(this)}/>
       </div>
     )
+  }
+  createTask(task){
+    this.state.todos({
+      task,
+      isCompleted: false
+    })
+    this.setState({ todos: this.state.todos})
   }
 }
 
