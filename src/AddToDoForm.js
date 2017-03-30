eb9d04e052372a1fd24b1870b7f6374f57a8fbb8
Travis Batts <Travisbatts6@gmail.com>
2017-03-30 09:08:16 -0400

    fist save

---
+++ src/AddToDoForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+ class AddTodoForm extends Component {
+   render() {
+     if(!this.props.addTodo){
+       return(
+         <button onClick={(e) => this.props.toggleNewTodo(e) }>Add Todo</button>
+       )
+     } else {
+       return(
+         <form onSubmit={(e) => this.props.addNewTodo(e)}>
+           <input type="text" placeholder="Title" onChange={(e)} =>
+       this.props.setNewTodoTitle(e)}/>
+           <input type="text" placeholder="Details" onChange{(e)} =>
+       this.props.setNewTodoBody(e)}/>
+           <button type="submit">Add Todo</button>
+         </form>
+       )
+     }
+   }
+ }
+
+ export default AddTodoFor
