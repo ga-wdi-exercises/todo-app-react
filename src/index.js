c49c8927551236533c7e47d9810c70ae2083b6f8
xgironx <xgironx@gmail.com>
2017-03-29 21:53:36 -0400

    initcommit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './App.js';
+
+const list = {
+  title:            "Things I Need To do",
+  stillTodos:   [ "Plan Capstone project", "Finish Capstone project", "Brush up resume", "Schedule interviews" ]
+  ,
+  completedTodos: [ "Finish last homework"
+  ]
+};
+
+ReactDOM.render(
+  <   TodoList
+        title={list.title}
+        stillTodos={list.stillTodos}
+        completedTodos={list.completedTodos}
+  />
+  ,
+  document.getElementById('root')
+);
