4b65839902557768f6b23cbd3fdfa0c6c91cc8c3
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-29 20:59:41 -0400

    todoapp

---
+++ src/Todo.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+
+class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      completed: this.props.completed
+    }
+  }
+
+  markComplete(){
+    this.setState({
+      completed: true
+    })
+  }
+  render() {
+    let {title, body} = this.props.todo
+    console.log(title, body);
+    if(this.state.completed) {
+      return(
+        <div className='todo'>
+        <div>
+        <h3>{ title }</h3>
+        <p>{ body }</p>
+        </div>
+        <div>
+          done!
+        </div>
+      <div>
+        <button value={this.props.index} onClick={(e) => this.props.removeTodo(e)}>Remove</button>
+          </div>
+        </div>
+      )
+    } else {
+      return(
+        <div className='todo'>
+        <div>
+        <h3>{ title }</h3>
+        <p>{ body }</p>
+        </div>
+          <div>
+            <button onClick={() => this.markComplete() }></button>
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+export default Todo
