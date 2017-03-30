41edea2f44001b611e84b3f6314ccaea45f4c94e
Malcom Johnson <malcom7johnson@gmail.com.com>
2017-03-30 01:40:53 -0400

    Did my Best

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Developer TODO: add application entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from './App';
+import './index.css';
+
+var todos = {
+  items: [
+  "Update GitHub Portfolio",
+  "Project 4 ideas",
+  "Vet appointments"
+]}
+
+ReactDOM.render(
+  <Todo items={todos.items}/>,
+  document.getElementById('root')
+);
