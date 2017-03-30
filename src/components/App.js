9a2c0b8c3d35188e14d5e1d47ef96477027a7278
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-30 08:24:56 -0400

    save function added, doesn't work

--- src/components/App.js
+++ src/components/App.js
@@ -14,14 +14,12 @@ const todos = [
   {
     task: 'learn redux',
     isCompleted: false
-
   },
   {
     task: 'go to meetup',
     isCompleted: true
   }
 ]
-
 class App extends Component {
   constructor(props){
     super(props)
@@ -29,22 +27,36 @@ class App extends Component {
       todos:todos
     }
   }
+  createTask(task){
+    this.state.todos.push({
+      task:task,
+      isCompleted: false
+    })
+
+    this.setState({
+      todos: this.state.todos
+    })
+  }
+
+  saveTask(oldTask, newTask){
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask)
+    foundTodo.task = newTask
+
+    this.setState({
+      todos: this.state.todos
+    })
+  }
   render(){
     return(
       <div>
         <h1>React Todo App</h1>
           <CreateTodo createTask={this.createTask.bind(this)}/>
-          <TodosList todos={this.state.todos} createTask={this.createTask.bind(this)}/>
+          <TodosList todos={this.state.todos} createTask={this.createTask.bind(this)} saveTask={this.saveTask.bind(this)}/>
+
       </div>
     )
   }
-  createTask(task){
-    this.state.todos.push({
-      task,
-      isCompleted: false
-    })
-    this.setState({ todos: this.state.todos})
-  }
+
 }
 
 
--- src/components/TodosListItem.js
+++ src/components/TodosListItem.js
@@ -8,12 +8,46 @@ class TodosListItem extends Component {
       isEditing: false
     }
   }
+  onEditClick(){
+    this.setState({
+      isEditing: true
+    })
+  }
+  onCancelClick(){
+    this.setState({
+      isEditing: false
+    })
+  }
+  onSaveClick(e){
+    e.preventDefault()
+
+    const oldTask = this.props.task
+    const newTask = this.refs.editInput.value
+    this.props.saveTask(oldTask, newTask)
+    this.setState({
+      isEditing: false
+    })
+  }
+
+  showTaskSection(){
+    const {task} = this.props;
+
+    if(this.state.isEditing){
+      return(
+        <td>
+          <form onSubmit={this.onSaveClick.bind(this)}>
+            <input type="text" defaultValue={task} ref="editInput" />
+          </form>
+        </td>
+      )
+    }
+  }
 
   renderActionsSection(){
     if(this.state.isEditing){
       return(
         <td>
-          <button>Save</button>
+          <button onClick={this.onSaveClick.bind(this)}>Save</button>
           <button onClick={this.onEditClick.bind(this)}>Cancel</button>
         </td>
       )
@@ -27,21 +61,18 @@ class TodosListItem extends Component {
   }
 render(){
   return(
-        <thead>
           <tr>
-            <td>{this.props.task}</td>
+            <td>
+              {this.props.task}
+            </td>
+            <td>
+              {this.showTaskSection()}
+            </td>
             <td>
               {this.renderActionsSection()}
             </td>
           </tr>
-        </thead>
       )
-    }
-    onEditClick(){
-      this.setState({isEditing: true})
-    }
-    onCancelClick(){
-      this.setState({isEditing: false })
     }
   }
 
