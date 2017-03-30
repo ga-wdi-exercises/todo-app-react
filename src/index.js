bbed3ed0d2363504675c3efe4c82bf6205595e6e
Chris Wightman <wightman_christopher@bah.com>
2017-03-30 08:13:32 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-let message = "Hello from Entry!!!!";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './App.js';
 
-console.log(message)
+const list = {
+  title: "Chores",
+  todos: [
+    "Laundry",
+    "Mow Lawn",
+    "Fix Shower",
+    "Clean House",
+    "Find Lawyer"
+  ],
+  completeTodos: [
+    "Make Todo App"
+  ]
+};
+
+ReactDOM.render(
+  <List title={list.title}
+  todos={list.todos}
+  completeTodos={list.completeTodos} />,
+  document.getElementById('root')
+);
